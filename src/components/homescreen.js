4412e06511142565ce75a74556f06e26e585e909
Ballred95 <blake.allred95@icloud.com>
2020-07-07 23:24:31 -0600

    added homescreen file and style file for it

---
+++ src/components/homescreen.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+export default function Homescreen(props) {
+    return (
+    <div className='home-screen-wrapper'>
+        <div className = 'controls'></div>
+    </div>
+    )
+}
