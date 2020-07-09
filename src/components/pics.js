52818c483538418415986af015609ebd1fa818ec
Ballred95 <blake.allred95@icloud.com>
2020-07-09 16:57:40 -0600

    testing pics box and search input styling. WOK

--- src/components/pics.js
+++ src/components/pics.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Pics(props) {
     return (
-    <div className='pics'>
-        
+    <div className='pics-container'>
+        <input className = 'imginput' type="text" placeholder = 'search backgrounds'/>
     </div>
     )
 }
