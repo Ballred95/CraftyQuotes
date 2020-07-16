9841ff37b9cde59e6b3430ca917ecd5c4d090560
Ballred95 <blake.allred95@icloud.com>
2020-07-16 15:46:35 -0600

    test didn't work reverted back

--- src/components/app.js
+++ src/components/app.js
@@ -1,52 +1,15 @@
 import React, { Component } from 'react';
-import { Emitter } from 'raycast-dom';
-
 import Homescreen from './homescreen';
 
 
 
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-        circles: [],
-        selectedCircleIndex: -1
-    };
-
-    // Emitter subscription
-    Emitter.getInstance().connect({
-        onMouseOver: this.onMouseOver,
-        onMouseOut: this.onMouseOut
-    });
-}
-
-onMouseOver(ray) {
-    var circle = ray.intersectsId(CIRCLE_ID_PREFIX),
-        circleId, circleIndex;
-
-    if (circle) { // circle mouse over            
-        circleId = circle.id;
-        circleIndex = parseInt(circleId.split(CIRCLE_ID_PREFIX)[1]);
-        this.setState({
-            hoveredCircleIndex: circleIndex
-        });
-    }
-}
-
-onMouseOut(ray) {
-    if (ray.intersectsId(CIRCLE_ID_PREFIX)) {
-        // circle mouse over
-        this.setState({
-            hoveredCircleIndex: -1
-        });
-    }
-}
+  
   render() {
     return (
       <div className='app'>
-        {/* <Homescreen /> */}
+        <Homescreen />
        
       </div>
     );
