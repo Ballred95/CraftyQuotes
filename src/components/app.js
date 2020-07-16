3fc1f198c9df6cede3db7d915cd321161a90d31e
Ballred95 <blake.allred95@icloud.com>
2020-07-16 15:18:57 -0600

    drag drop test didn't work on mobile, reverted back

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-// import Homescreen from './homescreen';
-import Board from './board';
-import Card from './card';
+import Homescreen from './homescreen';
+
 
 
 
@@ -9,18 +8,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        {/* <Homescreen /> */}
-        <Board id='board-1' className='board'>
-        <Card id ='card-1' className='card' draggable='true'>
-        <p>card one</p>
-        </Card>
-        </Board>
-
-        <Board id='board-2' className='board'>
-        <Card id ='card-2' className='card' draggable='true'>
-        <p>card two</p>
-        </Card>
-        </Board>
+        <Homescreen />
+       
       </div>
     );
   }
