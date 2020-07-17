bc61cc6216a20123c5e097acb82c359d0b448d0a
Ballred95 <blake.allred95@icloud.com>
2020-07-17 14:27:04 -0600

    refactored code and styled images correctly for mobile

--- src/components/image_list/imagelist.js
+++ src/components/image_list/imagelist.js
@@ -1,11 +1,23 @@
 import React from 'react' 
 
+
 export default function Imagelist(props) {
     return (
-    <div className='image_container'>
+    <div className='container'>
+        <div className = 'row'>
         {props.images.map(image => {
-            return <button key={image.id} onClick= {props.clh}><img className='imgs' src={image.previewURL} /></button>
+            return (<div key={image.id} className="col-md-4" style={{ marginBottom:"2rem" }}>
+                <div className = 'imageList_container'>
+                <img className='imageList_image' src={image.largeImageURL} />
+                </div>
+                <div className = 'image_details'>
+                    <button>Select</button>
+                </div>
+            </div>)
         })}
+
+        </div>
+       
     </div>
     )
 }
