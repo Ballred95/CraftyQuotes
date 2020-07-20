9d85f14a01106f3e9ab88ad80eaa3e2cef5cf8f6
Ballred95 <blake.allred95@icloud.com>
2020-07-19 20:26:06 -0600

    WOK

--- src/components/imageview/imageview.js
+++ src/components/imageview/imageview.js
@@ -1,12 +1,41 @@
-import React from 'react' 
+import React, {Component} from 'react' 
+import Pics from '../tools/pics'
+import Clipart from '../tools/clipart'
+import Fonts from '../tools/fonts'
+import PicRender from '../picrender'
 
-export default function Imageview(props) {
 
-    console.log(props.location.state.image)
-    const {largeImageURL: image, user, pageURL} = props.location.state.image
-    return (
-    <div className=''>
-        <img src={image} />
-    </div>
-    )
-}
+
+
+
+export default class Imageview extends Component {
+    constructor(props){
+        super(props)
+        const {largeImageURL: image, user, pageURL} = props.location.state.image
+
+        this.state = {
+            picRender: <img src={image} />
+        }
+
+    }
+
+    
+    render() {
+        return (
+        <div className='home-screen-wrapper'>
+            {/* <Workstation rendering = {this.state.picRender} /> */}
+            <div className ='pic-render'>{this.state.picRender}</div>
+            <div className='tools'>
+                <Pics renderFunction = {()=> this.setState({picRender: <PicRender  />})}/>
+                <Fonts renderFunction = {()=> this.setState({picRender: 'what font u want?'})} />
+                <Clipart renderFunction = {()=> this.setState({picRender: 'what clipart u want?'})} />
+            
+            </div> 
+            <div className='buttons'>
+            <button>Save</button>
+            <button>Export</button> 
+            </div>
+        </div>
+        )
+    }
+    }
