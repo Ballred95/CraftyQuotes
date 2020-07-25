21bf0be1603821d5ed28e79a9190110f06580e9f
Ballred95 <blake.allred95@icloud.com>
2020-07-25 13:21:44 -0600

    added align icons

--- src/components/tools/fonts.js
+++ src/components/tools/fonts.js
@@ -42,9 +42,21 @@ render() {
             <div>
                 <h3>Type {this.state.finalValue}</h3>
                     <div className='alignment'>
-                        <button className='left-button' onClick={()=>this.setState({alignment: "font-state-left"})} >Left</button>
-                        <button className='center-button' onClick={()=>this.setState({alignment: "font-state-center"})} >Center</button>
-                        <button className='right-button' onClick={()=>this.setState({alignment: "font-state-right"})} >Right</button>
+                        <button className='left-button' onClick={()=>this.setState({alignment: "font-state-left"})} ><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-align-start" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M1.5 1a.5.5 0 0 1 .5.5v13a.5.5 0 0 1-1 0v-13a.5.5 0 0 1 .5-.5z"/>
+                        <path d="M3 7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7z"/>
+                        </svg></button>
+                        <button className='center-button' onClick={()=>this.setState({alignment: "font-state-center"})} >
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-align-center" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8 1a.5.5 0 0 1 .5.5V6h-1V1.5A.5.5 0 0 1 8 1zm0 14a.5.5 0 0 1-.5-.5V10h1v4.5a.5.5 0 0 1-.5.5zM2 7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7z"/>
+</svg>
+                        </button>
+                        <button className='right-button' onClick={()=>this.setState({alignment: "font-state-right"})} >
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-align-end" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M14.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 1 0v-13a.5.5 0 0 0-.5-.5z"/>
+  <path d="M13 7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V7z"/>
+</svg>
+                        </button>
 
                     </div>
                 
